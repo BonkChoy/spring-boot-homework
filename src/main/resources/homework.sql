28f80546f3769c490af52a7808e3585fd7c80afe
shuaihua <1252608364@qq.com>
2019-12-05 22:33:42 +0800

    spring-boot-homework-tag5

--- src/main/resources/homework.sql
+++ src/main/resources/homework.sql
@@ -7,11 +7,11 @@
  Source Host           : localhost:3306
  Source Schema         : homework
 
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50725
+ File Encoding         : 65001
 
- Date: 25/06/2019 23:18:02
+ Date: 05/12/2019 22:32:12
 */
 
 SET NAMES utf8mb4;
@@ -35,13 +35,14 @@ CREATE TABLE `category`  (
   `created` datetime(0) NULL DEFAULT NULL COMMENT '创建日期',
   `modified` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of category
 -- ----------------------------
 INSERT INTO `category` VALUES (1, '首页', NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL);
 INSERT INTO `category` VALUES (2, '动态', NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `category` VALUES (3, '问答', NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for comment
@@ -89,11 +90,11 @@ CREATE TABLE `post`  (
 -- Records of post
 -- ----------------------------
 INSERT INTO `post` VALUES (1, '这是标题，标题', '内容，这是内容', '0', 1, 1, 0, 0, 140, 0, 0, 1, 0, '2018-10-15 13:27:35', '2018-10-13 16:33:07');
-INSERT INTO `post` VALUES (2, '这是标222222222222222222', '内容，这是内容', '0', 2, 1, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
-INSERT INTO `post` VALUES (3, '这是标222222222222222222', '内容，这是内容', '0', 2, 1, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
-INSERT INTO `post` VALUES (4, '这是标222222222222222222', '内容，这是内容', '0', 2, 1, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
-INSERT INTO `post` VALUES (5, '这是标222222222222222222', '内容，这是内容', '0', 2, 1, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
-INSERT INTO `post` VALUES (6, '这是标222222222222222222', '内容，这是内容', '0', 2, 1, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
+INSERT INTO `post` VALUES (2, '这是标222222222222222222', '内容，这是内容', '0', 2, 2, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
+INSERT INTO `post` VALUES (3, '这是标222222222222222222', '内容，这是内容', '0', 2, 2, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
+INSERT INTO `post` VALUES (4, '这是标222222222222222222', '内容，这是内容', '0', 2, 2, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
+INSERT INTO `post` VALUES (5, '这是标222222222222222222', '内容，这是内容', '0', 2, 2, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
+INSERT INTO `post` VALUES (6, '这是标222222222222222222', '内容，这是内容', '0', 2, 2, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
 INSERT INTO `post` VALUES (7, '这是标222222222222222222', '内容，这是内容', '0', 2, 1, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
 INSERT INTO `post` VALUES (8, '这是标222222222222222222', '内容，这是内容', '0', 2, 1, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
 INSERT INTO `post` VALUES (9, '这是标222222222222222222', '内容，这是内容', '0', 2, 1, 0, 0, 67, 99, 1, 1, 0, '2018-10-15 13:27:26', '2018-10-13 16:33:07');
@@ -116,7 +117,7 @@ CREATE TABLE `test_user`  (
 -- ----------------------------
 -- Records of test_user
 -- ----------------------------
-INSERT INTO `test_user` VALUES (1, 18, '小明', '123@qq.com');
+INSERT INTO `test_user` VALUES (1, 123, '小明', '123@qq.com');
 
 -- ----------------------------
 -- Table structure for user
@@ -129,8 +130,11 @@ CREATE TABLE `user`  (
   `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮件',
   `mobile` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机电话',
   `point` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '积分',
+  `sign` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '个性签名',
   `gender` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '性别',
   `wechat` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '微信号',
+  `vip_level` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'vip等级',
+  `modified` datetime(0) NULL DEFAULT NULL COMMENT '最后修改时间',
   `birthday` datetime(0) NULL DEFAULT NULL COMMENT '生日',
   `avatar` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '头像',
   `post_count` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '内容数量',
@@ -138,13 +142,13 @@ CREATE TABLE `user`  (
   `status` tinyint(2) NOT NULL DEFAULT 0 COMMENT '状态',
   `lasted` datetime(0) NULL DEFAULT NULL COMMENT '最后的登陆时间',
   `created` datetime(0) NOT NULL COMMENT '创建日期',
-  `modified` datetime(0) NULL DEFAULT NULL COMMENT '最后修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'xiaoming', '1111', NULL, NULL, 0, NULL, NULL, NULL, 'https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg', 0, 0, 0, NULL, '2018-10-14 18:41:34', NULL);
+INSERT INTO `user` VALUES (1, 'xiaoming', '1111', NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, 'https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg', 0, 0, 0, NULL, '2018-10-14 18:41:34');
+INSERT INTO `user` VALUES (2, 'shuaihua', '7a977024ceae22a499a80d9ea208ab34', 'admin@qq.com', NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, '/images/avatar/default.png', 0, 0, 0, '2019-12-05 22:06:05', '2019-07-14 22:24:37');
 
 SET FOREIGN_KEY_CHECKS = 1;
