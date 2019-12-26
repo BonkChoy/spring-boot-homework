d7dec349b13e2935fd42d4caf42c1c0b59bac265
caiwen <13126031938@163.com>
2019-12-26 18:07:20 +0800

    spring-boot-homework-tag7

--- src/main/java/com/homework/controller/PostController.java
+++ src/main/java/com/homework/controller/PostController.java
@@ -2,19 +2,22 @@ package com.homework.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.api.R;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.homework.entity.Category;
 import com.homework.entity.Post;
 import com.homework.service.CategoryService;
+import com.homework.utils.Constant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -71,5 +74,42 @@ public class PostController extends  BaseController{
 
         return "post/add";
     }
+
+    @ResponseBody
+    @PostMapping("/user/post")
+    public R posttPost(@Valid Post post, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            return R.failed(bindingResult.getFieldError().getDefaultMessage());
+        }
+
+        if(post.getId() == null) {
+            post.setUserId(getProfileId());
+
+            post.setModified(new Date());
+            post.setCreated(new Date());
+            post.setCommentCount(0);
+            post.setEditMode(Constant.EDIT_HTML_MODEL);
+            post.setLevel(0);
+            post.setRecommend(false);
+            post.setViewCount(0);
+            post.setVoteDown(0);
+            post.setVoteUp(0);
+            post.setStatus(Constant.NORMAL_STATUS);
+        } else {
+
+            Post tempPost = postService.getById(post.getId());
+            if(tempPost.getUserId() != getProfileId()) {
+                return R.failed("不是自己的帖子");
+            }
+        }
+
+        postService.saveOrUpdate(post);
+
+        //TODO 给所有订阅的人发送消息
+
+        return R.ok(post.getId());
+    }
+
 
 }
